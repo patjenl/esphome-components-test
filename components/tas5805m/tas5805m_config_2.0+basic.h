34a8b2514f03248c1d9aa60081e49933fa051379
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-03-01 19:56:32 +1000

    updates

--- components/tas5805m/tas5805m_config_2.0+basic.h
+++ components/tas5805m/tas5805m_config_2.0+basic.h
@@ -1142,7 +1142,7 @@ static const tas5805m_cfg_reg_t tas5805m_registers[] = {
   { 0x30, 0x00 },
   { 0x4c, 0x14 }, //   @mrtoy-me changed to digital volume 14db (20 decimal) to suit esphome arduino
   { 0x53, 0x00 },
-  { 0x54, 0x1F }, //   @mrtoy-me changed to analog gain -15db (31 decimal) to suit esphome arduino and idf
+  { 0x54, 0x1F }, //   @mrtoy-me changed to analog gain -15db (31 decimal) to suit esphome idf while working with esphome arduino
   { 0x03, 0x03 },
   { 0x78, 0x80 },
 };
